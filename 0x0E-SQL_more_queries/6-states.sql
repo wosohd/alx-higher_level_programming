8d8637ae0f64a8f1d3826dfad5b9f696ddf66194
wosohd <wosohd@gmail.com>
2023-07-19 09:46:49 -0700

    -mmore SQL Queries

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_usa and the table states on your MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IFNOT EXISTS states (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
